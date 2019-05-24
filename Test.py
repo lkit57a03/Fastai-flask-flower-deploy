45bb6284c328c2d781db6cdba64de441974c9833
Harsh <lkit57a03@gmail.com>
2019-05-24 09:07:53 +0000

    Changes the app.py file added code to handle my model

--- Test.py
+++ Test.py
@@ -1,6 +1,6 @@
 from fastai.vision import *
 path = Path('models')
-img = open_image(path/'102841525_bd6628ae3c.jpg')
+img = open_image(path/'image.jpg')
 learn = load_learner(path)
 pred_class,pred_idx,outputs = learn.predict(img)
-print(pred_class)
+print(str(pred_class))
